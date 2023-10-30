ee061aedf4366dd80cf2995ab3460312262d2a48
slobodianiukdenys <144223224+slobodianiukdenys@users.noreply.github.com>
2023-10-31 01:42:27 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+//Введіть бажану довжину масиву
+const arrayLength = 2;
+
+//Введіть число з якого розпочнеться масив
+const startNumber = 1;
+//---------
+let arr1 = undefined;
+
+    if (arrayLength >= 1){
+        arr1 = new Array(arrayLength);
+    }
+    else{
+        arr1 = 'Введіть довжину масиву більшу за 0'
+    }  
+
+    let i = startNumber;
+    let j = 0;
+
+    do{
+		arr1[j] = +i;
+        i++;
+		j++;
+	}while (j < arrayLength);
+
+    console.table(arr1)
